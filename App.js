b6bd1caf8b36c66d649126fbe186aaba2df9ab05
Lucy Sutton <Theodore@Lucys-Mac-mini.local>
2017-11-10 15:17:10 -0500

    taking a different approach

--- App.js
+++ App.js
@@ -1,66 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-// import React, { Component } from 'react';
-// import {
-//   Platform,
-//   StyleSheet,
-//   Text,
-//   View,
-//   Button,
-//   Alert,
-// } from 'react-native';
-
-
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' +
-//     'Cmd+D or shake for dev menu',
-//   android: 'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu',
-// });
-
-// export default class App extends Component<{}> {
-//   render() {
-//     let date = new Date();
-//     let dateString = date.getHours().toString() + ':' + date.getMinutes().toString();
-//     return (
-      
-//       <View style={{height: 300}}>
-//         <View style={{flex: 1, backgroundColor: 'powderblue'}} />
-//         <View style={{flex: 1, backgroundColor: 'skyblue'}} />
-//         <View style={{flex: 1, backgroundColor: 'steelblue'}} />
-//         <Text style={{fontSize: 45}}>{dateString}</Text>
-//       </View>
-      
-//     );
-//   }
-// }
-
-
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 45,
-//     textAlign: 'center',
-//     color: 'pink',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-
 import React, {Component} from 'react';
 import {
 	View,
@@ -72,7 +9,7 @@ import {
 
 import moment, { diff } from 'moment';
 import timer from 'moment-timer';
-import ReactMomentCountDown from 'react-moment-countdown'
+//import ReactMomentCountDown from 'react-moment-countdown'
 
 const styles = StyleSheet.create({
 	container: {
@@ -119,7 +56,6 @@ export default class App extends Component {
         <Text style={styles.dateText}>Time to Leave:</Text>
         <Text style={styles.timeText}>{moment(this.state.timeToLeave).format('LTS')}</Text>
         <Text style={styles.dateText}>Time Remaining:</Text>
-        <Text style={styles.timeText}>{moment.duration(this.state.timeRemaining).format('LTS')}</Text>
        
 			</View>
 		)
