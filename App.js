186bd0adfee47d162a8ab4f87ffaeec1caa6b3f0
Lucy Sutton <Theodore@Lucys-Mac-mini.local>
2017-11-10 15:09:23 -0500

    still working on timer

--- App.js
+++ App.js
@@ -70,7 +70,9 @@ import {
 } from 'react-native';
 
 
-import moment from 'moment';
+import moment, { diff } from 'moment';
+import timer from 'moment-timer';
+import ReactMomentCountDown from 'react-moment-countdown'
 
 const styles = StyleSheet.create({
 	container: {
@@ -96,28 +98,34 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			time: moment().format("LTS"),
-			date: moment().format("LL")
+			time: moment(),
+      timeToLeave: new Date(2017, 11, 10, 16, 0, 0, 0),
+      timeRemaining: new Date(2017, 11, 10, 16, 0, 0, 0) - Date.now()
 		};
 	}
 	render() {
 		setTimeout(() => {
 			this.setState({
-				time: moment().format("LTS"),
-				date: moment().format("LL")
+				time: moment(),
+        timeRemaining: new Date(2017, 11, 10, 16, 0, 0, 0) - Date.now(),
 			});
-		}, 1000);
+    }, 1000);
 
 		return (
 			<View style={styles.container}>
         <StatusBar style={styles.status} />
-				<Text style={styles.timeText}>
-					{this.state.time}
-				</Text>
-				<Text style={styles.dateText}>
-					{this.state.date}
-				</Text>
+        <Text style={styles.dateText}>Current Time/Date:</Text>
+				<Text style={styles.timeText}>{this.state.time.format('LTS')}</Text>
+        <Text style={styles.dateText}>Time to Leave:</Text>
+        <Text style={styles.timeText}>{moment(this.state.timeToLeave).format('LTS')}</Text>
+        <Text style={styles.dateText}>Time Remaining:</Text>
+        <Text style={styles.timeText}>{moment.duration(this.state.timeRemaining).format('LTS')}</Text>
+       
 			</View>
 		)
 	}
 }
+// eventTime = moment('27-11-2020 08:30:00', 'DD-MM-YYYY HH:mm:ss').unix(),
+// currentTime = moment().unix(),
+// diffTime = eventTime - currentTime,
+// duration = moment.duration(diffTime * 1000, 'milliseconds'),
