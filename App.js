5d7fec56585549befc67fceca814c2e2da5d7edb
Lucy Sutton <Theodore@Lucys-Mac-mini.local>
2017-11-10 16:50:37 -0500

    unformatted countdown implemented

--- App.js
+++ App.js
@@ -35,28 +35,32 @@ export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			time: moment(),
-      timeToLeave: new Date(2017, 11, 10, 16, 0, 0, 0),
-      timeRemaining: new Date(2017, 11, 10, 16, 0, 0, 0) - Date.now()
+			time: new Date(),
+			timeToLeave: new Date(2017, 10, 10, 19, 0, 0, 0),
+      //timeRemaining: this.timeToLeave.getTime() - this.time.getTime()
 		};
 	}
 	render() {
 		setTimeout(() => {
 			this.setState({
-				time: moment(),
-        timeRemaining: new Date(2017, 11, 10, 16, 0, 0, 0) - Date.now(),
+				time: new Date()
 			});
-    }, 1000);
+		}, 1000);
 
+		const timeRemaining = this.state.timeToLeave.getTime() - this.state.time.getTime();
+		const mom = moment.duration(timeRemaining).toISOString();
+		const hours = mom.slice(mom.indexOf('T') + 1, mom.indexOf('T') + 2);
+		const minutes = mom.slice(mom.indexOf('H') + 1, mom.indexOf('T') + 3);
+		const formatted = minutes
 		return (
 			<View style={styles.container}>
         <StatusBar style={styles.status} />
         <Text style={styles.dateText}>Current Time/Date:</Text>
-				<Text style={styles.timeText}>{this.state.time.format('LTS')}</Text>
+				<Text style={styles.timeText}>{moment(this.state.time).format('LLLL')}</Text>
         <Text style={styles.dateText}>Time to Leave:</Text>
-        <Text style={styles.timeText}>{moment(this.state.timeToLeave).format('LTS')}</Text>
+        <Text style={styles.timeText}>{moment(this.state.timeToLeave).format('LLLL')}</Text>
         <Text style={styles.dateText}>Time Remaining:</Text>
-       
+				<Text style={styles.timeText}>{mom}</Text>
 			</View>
 		)
 	}
